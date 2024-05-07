292c085204fce326d558a6811d0e0f3173e438ed
Fernanda Barros Linhares <fernanda_linhares@estudante.sesisenai.org.br>
2024-05-07 11:05:43 -0300

    Aplicando solid, separando a usuario (rota e controllers)

--- src/routes/usuario.router.js
+++ src/routes/usuario.router.js
@@ -1,37 +1,13 @@
 const { Router } = require("express");
-const Usuario = require('../models/Usuario');
-const {sign} = require('jsonwebtoken');
+// const Usuario = require('../models/Usuario');
+// const {sign} = require('jsonwebtoken');
+const UsuarioController = require("../controllers/UsuarioController");
 //Importar o auth quando criar as rotas get
 // const validacaoUsuarios = require('../middleware/validacaoUsuarios'); com a rotas juntas
 
 const usuarioRoutes = new Router()
 
 //Rota Usuario
-usuarioRoutes.post('/',async  (req, res) => {
-
-    try {
-        const { nome, sexo, cpf, email, senha, data_nascimento, endereco } = req.body;
-
-        if(!nome || !sexo || !cpf || !email || !senha || !data_nascimento ||!endereco) {
-            return res.status(400).json({messagem:'Dados obrigatórios'})
-        }
-
-        const usuario = await Usuario.create({
-            nome,
-            sexo,
-            cpf,
-            email,
-            senha,
-            data_nascimento,
-            endereco
-        }) ;
-        
-
-        res.status(201).json(usuario);
-        
-     } catch (error) {
-        res.status(500).json({ error: 'Não possível cadastrar o usuário' })
-    }
-})
+usuarioRoutes.post('/', UsuarioController.cadastrar)
 
 module.exports = usuarioRoutes
