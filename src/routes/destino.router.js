4522bf1b94b473ab5d0604fdb1ab90b1c7478400
Fernanda Barros Linhares <fernanda_linhares@estudante.sesisenai.org.br>
2024-05-07 16:11:41 -0300

    Separacao do crud de login e limpeza em dados copiados

--- src/routes/destino.router.js
+++ src/routes/destino.router.js
@@ -1,11 +1,10 @@
 const { Router } = require("express");
-// const Destino = require("../models/Destino");
-//impprtar auth
+
 const DestinoController = require("../controllers/DestinoController");
 
 const destinoRoutes = new Router()
 
-//Rota destinos
+
 
 destinoRoutes.post('/', DestinoController.cadastrar)
 
--- src/routes/usuario.router.js
+++ src/routes/usuario.router.js
@@ -1,13 +1,11 @@
 const { Router } = require("express");
-// const Usuario = require('../models/Usuario');
-// const {sign} = require('jsonwebtoken');
+
 const UsuarioController = require("../controllers/UsuarioController");
-//Importar o auth quando criar as rotas get
-// const validacaoUsuarios = require('../middleware/validacaoUsuarios'); com a rotas juntas
+
 
 const usuarioRoutes = new Router()
 
-//Rota Usuario
+
 usuarioRoutes.post('/', UsuarioController.cadastrar)
 
 module.exports = usuarioRoutes
