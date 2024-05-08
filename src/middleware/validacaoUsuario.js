fc93308ca970971f354e5fcf99222697aad7c923
Fernanda Barros Linhares <fernanda_linhares@estudante.sesisenai.org.br>
2024-05-08 15:05:21 -0300

    fix-Validacao usuario

---
+++ src/middleware/validacaoUsuario.js
@@ -0,0 +1,25 @@
+const yup = require('yup')
+
+const schema = yup.object({
+    nome: yup.string().required(),
+    sexo:yup.string().required(),
+    cpf:yup.string().required(),
+    email:yup.string().required(),
+    senha:yup.string().required(),
+    data_nascimento:yup.string().required(),
+    endereco:yup.string().required(),
+});
+const validacaoUsuarios = async(req, res, next)=>{
+    const {body} = req;
+
+    try {
+       
+        await schema.validate(body, { abortEarly: false });
+        
+      } catch (erro) {
+        
+        res.status(400).json({ erro: erro.errors });
+      }
+      next();
+    };
+    module.exports = {validacaoUsuarios}
