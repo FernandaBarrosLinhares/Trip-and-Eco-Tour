92a1d6cd22fac902e8218c10f88ba3cc99a34f17
FernandaBarrosLinhares <124844643+FernandaBarrosLinhares@users.noreply.github.com>
2024-05-09 20:47:29 -0300

    Revert "Feature listar destino"

--- src/routes/login.router.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const LoginController = require("../controllers/LoginController");
-
-const loginRoutes = new Router()
-
-loginRoutes.post('/', LoginController.login )
-
-
-module.exports = loginRoutes
